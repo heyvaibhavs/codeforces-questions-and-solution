1b8efc0654ba78f3d77c68c91f1d2dd5b1e73250
sam-072 <shyamprakash807@gmail.com>
2021-09-01 20:59:08 +0530

    password question of codeforce

--- passwords.py
+++ passwords.py
@@ -3,23 +3,25 @@
 # rating : 1100
 
 n,k=map(int,input().split())
-l,s=[],[]
-for i in range(n+1):
-    s1=input()
-    if i==0:
-        s.append(s1)
-        l.append(len(s1))
-    elif i>0 and  s[-1]!=s1:
-        l.append(len(s1))
-        s.append(s1)
-n=len(s)-1
-
-if len(s)==1:
+l=[]
+for i in range(n):
+    s=input()
+    l.append(len(s))
+s=input()
+if n==1:
     print(1,1)
-elif len(set(l))==1:
-    print(1,(n//k)*5+n)
-else:
-    for i in range(len(l)-1,0,-1):
-        if l[i]==l[i-1]:
-            print(i,(n//k)*5+n)
-            break
+    exit()
+l.sort()
+h1=l.index(len(s))
+h2=-1
+for i in range(h1+1,n):
+    if l[i]>len(s):
+        h2=i-1
+        break
+# print(h2)
+if h2==-1:
+    h2=n-1
+# print(h1,h2)
+h1=h1+1+(h1//k)*5
+h2=h2+1+(h2//k)*5   
+print(h1,h2)
