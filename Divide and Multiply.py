94503c0b9ea1ca7e0a310fdc02f437295767d0da
sam-072 <shyamprakash807@gmail.com>
2021-12-02 00:52:01 +0530

    Divide and Multiply codeforces solution in Python

--- Divide and Multiply.py
+++ Divide and Multiply.py
@@ -1,46 +1,118 @@
 # Code by : Sam._.072
 
-for _ in range(int(input())):
-    n = int(input())
-    a = list(map(int, input().split()))
-    z = sum(a)
-    a.sort()
-    b = a[:]
-    x, y = 0, 0
-    for i in range(n-1,-1,-1):
-        if a[i]%2!=0 :
-            x = a[i]
-            a.remove(a[i])
-            break
-    if x == 0:
-        x = a[-1]
-        a.remove(a[-1])
-    for i in range(n-1,-1,-1):
-        if b[i]%2==0 :
-            y = b[i]
-            b.remove(b[i])
-            break
-    if y == 0:
-        y = b[-1]
-        b.remove(b[-1])   
-    c1 = 0
-    for i in a:
-        if i%2!=0:
-            c1 += i
-        else:
-            while i%2 == 0:
-                i = i // 2
-                x = x * 2
-            c1 += i
-    c2 = 0
-    for i in b:
-        if i%2!=0:
-            c2 += i
-        else:
-            while i%2 == 0:
-                i = i // 2
-                y = y * 2
-            c2 += i
-    print(max(c1+x,z,c2+y))
+from __future__ import division, print_function
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+if sys.version_info[0] < 3:
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+if sys.version_info[0] < 3:
+    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+if __name__ == "__main__":
+
+    for _ in range(int(input())):
+        n = int(input())
+        a = list(map(int, input().split()))
+        arr = a[:]
+        h = sum(a)
+        b = []
+        for i in range(n):
+            if a[i]%2!=0:
+                b.append(1)
+            else:
+                c = 1
+                while a[i]%2==0:
+                    a[i] = a[i]//2
+                    c = c*2
+                b.append(c)
+        for i in range(n):
+            c = 0
+            x = arr[i]
+            for j in range(n):
+                if i!=j:
+                    if arr[j]%2!=0:
+                        c+=arr[j]
+                    else:
+                        c += a[j]
+                        x = x*b[j]
+            if c + x > h:
+                h= c + x
+        print(h)
+
+
+
+    
 
     
