9981e34351a576ba507236561ea5280bd95be32b
heyvaibhavs <85010295+heyvaibhavs@users.noreply.github.com>
2022-12-27 00:02:47 +0530

    Update Akku and Binary Number.java
    When I submit, my test case fails at 1 for 14 24 inputs showing your answer is 8 and correct answer is 4 but when I custom input 14 24, my output is also showing 4. I don't know why this happened.
    My code is running perfectly. 
    I tried many custom inputs and showing correct answer.
    I try very easy approach in this.
    Can you help.

--- Akku and Binary Numbers.py
+++ Akku and Binary Numbers.py
@@ -2,14 +2,48 @@
 # problem link : https://practice.geeksforgeeks.org/problems/akku-and-binary-numbers0902/0/
 # level : easy
 
-l,r=map(int,input().split())
-ans=[]
-for i in range(63):
-    x=1<<i
-    for j in range(i+1,63):
-        y=1<<j
-        for k in range(j+1,63):
-            v=(1<<k)|x|y
-            ans.append(v)
-ans.sort()
-print(br(ans,r)-bl(ans,l))
+public class AkkuBinaryNumber {
+    static int count = 0;
+    public static void main(String[] args) {
+        long L=14, R=24, a=0;
+
+        for(long i=L;i<=R;i++){
+
+            // System.out.println("Digit : "+i);
+            //Here a is receivibg final answer.
+            //converting every number to binary first between inputs
+            a = convertToBinary(i);
+        } 
+        System.out.println(a);
+    }
+
+    private static int convertToBinary(long N) {
+        int BinaryNum = 0;
+            int count = 0;
+            while (N != 0) {
+                long rem =  N% 2;
+                double c = Math.pow(10, count);
+                BinaryNum += rem * c;
+                N/= 2;
+                count++;
+            }
+        // System.out.println("Binary : "+BinaryNum);
+    
+        return checkOne(BinaryNum,1); //checking how many one present in binary number of inputs
+    }
+
+    private static int checkOne(int n, int d) {
+        int c = 0;
+     
+        while (n > 0)
+        {
+            if (n % 10 == d)c++;
+            n = n / 10;
+        }
+
+        if(c==3){       if binary number between inputs contains three 1s then it increase count by 1 everytime.
+            count++;
+        }
+        return count; 
+    }
+}
